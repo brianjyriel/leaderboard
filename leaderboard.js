98766f93f6090df1b3b7e3c95bfd0b962a0ca9c5
Brian Jyriel <bjyriel@gmail.com>
2015-10-27 10:11:28 -1000

    Finished tutorial until Events

--- leaderboard.js
+++ leaderboard.js
@@ -1,4 +1,4 @@
-if (Meteor.isClient) {
+/*if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
@@ -20,4 +20,28 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+}*/
+
+console.log("Hello World!");
+if(Meteor.isClient){
+  console.log("Hello client!");
+
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find()
+    },
+    'otherHelperFunction': function(){
+      return PlayersList.find()
+    }
+  });
+
+  Template.leaderboard.events({
+    'click': function(){
+      console.log("You clicked something");
+    }
+  });
+
 }
+
+
+PlayersList = new Mongo.Collection('players');
